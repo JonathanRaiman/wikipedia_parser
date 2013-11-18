c6ec0b0cb584ed1def7cd6a39c10aeac2d15e2d8
Jonathan Raiman <jonathanraiman@gmail.com>
2013-11-18 14:02:59 -0500

    added tests for resuming parsing of a node

--- tests/wikiParserStoppingTest.rb
+++ tests/wikiParserStoppingTest.rb
@@ -4,17 +4,24 @@ require(File.dirname(__FILE__)+"/../lib/wikipedia_parser.rb")
 describe WikiParser do
 	before(:each) do
 		@parse = WikiParser.new :path => File.dirname(__FILE__)+"/test_dump.xml"
+		@pages = []
 	end
 
 	it 'should only obtain the title when provided with that stopping point' do
-		pages = []
 		3.times do |i|
 			page = @parse.get_next_page :until => "title"
-			pages << page
+			@pages << page
 		end
-		pages.should have(3).items
-		pages.each do |page|
+		@pages.should have(3).items
+		@pages.each do |page|
 			page.internal_links.should be_empty
+		end
+	end
+
+	it 'should be able to resume parsing later' do
+		@pages.each do |page|
+			page.finish_processing
+			page.internal_links.should_not be_empty
 		end
 	end
 
